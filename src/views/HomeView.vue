dc14ebfcd6db8c7f0f98477251d27e36440499de
jeronimogarcia <jero_garcia23@hotmail.com>
2022-10-09 16:06:17 -0300

    Pinia, Navigation Guard, Firebase auth

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
-        Home
-    </div>
+  <div>
+    <h1>Home</h1>
+  </div>
 </template>
 
 <script setup>
+// import { userUserStore } from "../stores/userStore";
+// const userStore = userUserStore();
+// import {onAuthStateChanged} from 'firebase/auth'
+// import { auth } from '../FirebaseConfig/firebaseConfig';
 
+
+// onAuthStateChanged(auth, (user)=>{
+//   console.log(user)
+// })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
