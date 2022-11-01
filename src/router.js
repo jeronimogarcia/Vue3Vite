be912dc14db049af8b55c73e86af32d4fd1da436
jeronimogarcia <jero_garcia23@hotmail.com>
2022-11-01 15:10:40 -0300

    Error404

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import AboutView from './views/AboutView.vue'
 import DetailView from './views/DetailView.vue'
 import TicketsView from './views/TicketsView.vue'
 import CheckoutView from './views/CheckoutView.vue'
+import Page404View from './views/Page404View.vue'
 import {userUserStore} from './stores/userStore'
 
 const requireAuth = async(to, from, next) => {
@@ -30,6 +31,7 @@ const routes = [
   { path: "/product/:id", component: DetailView, beforeEnter: requireAuth },
   { path: "/checkout", component: CheckoutView, beforeEnter: requireAuth },
   { path: "/tickets", component: TicketsView,beforeEnter: requireAuth },
+  { path:'/:pathMatch(.*)*', component: Page404View }
 ];
 
 const router = createRouter({
---
+++ src/views/Page404View.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="mb-16 mt-10 flex justify-center">
+    <h1 class="font-semibold text-6xl">Fallo de routing</h1>
+  </div>
+  <div class="imgContainer">
+    <img src="../assets/error404.png" alt="">
+  </div>
+  <div class="flex justify-center mt-10">
+    <router-link to="/"
+        ><button  class="buttonStyle">Volver al Home</button></router-link
+      >
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+.imgContainer{
+  border: 1px solid black;
+  border-radius: 50px;
+  overflow: hidden;
+}
+
+img{
+    background-color: red;
+}
+
+.buttonStyle {
+  width: 300px;
+  background-color: #4187f6;
+  padding: 5px 0px 5px 0px;
+  border-radius: 15px;
+  color: white;
+  font-weight: 500;
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+}
+
+.buttonStyle:hover {
+  background-color: black;
+  color: yellow;
+}
+</style>
